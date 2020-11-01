db9c8057b2e9b7ec489ca6749c0caeb9afbcfdc1
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-11-01 23:24:45 +0400

    added tailwind

--- src/components/bhajans.vue
+++ src/components/bhajans.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="space-y-10">
-      <div v-for="(bhajan, value, index) in all" :key="value + index" class="shadow-lg">
+    <div class="grid grid-cols-3">
+      <div v-for="(bhajan, value, index) in all" :key="value + index" class="shadow-lg p-10">
         <p v-for="line in bhajan" :key="line">{{ line }}</p>
       </div>
     </div>
