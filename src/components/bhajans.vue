6f83120171d2449c92e14d4d59df3e18c3a38c47
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-11-02 12:53:49 +0400

    sticky

--- src/components/bhajans.vue
+++ src/components/bhajans.vue
@@ -4,17 +4,31 @@
     <pre>
       {{ results }}
     </pre> -->
-    <div class="flex items-center justify-center space-x-3">
-      <vue-fuse v-model="searchTerm" :keys="keys" :list="all" :threshold="threshold" :defaultAll="false" eventName="results" class="p-4 my-10 text-2xl text-center border border-gray-200 shadow-lg"></vue-fuse>
+    <div class="sticky top-0 flex items-center justify-center space-x-3">
+      <vue-fuse
+        v-model="searchTerm"
+        :keys="keys"
+        :list="all"
+        :threshold="threshold"
+        :defaultAll="false"
+        eventName="results"
+        class="p-4 my-10 text-2xl text-center border border-gray-200 shadow-lg"
+      ></vue-fuse>
       <!-- <input type="range" min="0.0" max="1.0" step="0.1" v-model="sensitivity" /> {{ sensitivity }} -->
       <!-- <button class="p-4 text-xs text-black bg-gray-500 rounded-lg outline" @click="runSearch">Search</button> -->
       <div>{{ results.length }} results</div>
     </div>
-    <div class="grid grid-cols-3 ">
-      <div v-for="(bhajan, value, index) in results" :key="bhajan.id" class="grid p-10 font-serif text-xl font-medium shadow-lg place-items-center">
+    <div class="grid md:grid-cols-3 ">
+      <div
+        v-for="(bhajan, value, index) in results"
+        :key="bhajan.id"
+        class="grid p-10 font-serif text-xl font-medium shadow-lg place-items-center"
+      >
         <div class="">
           <!-- <p>{{ bhajan.content }}</p> -->
-          <p v-for="(line, index) in bhajan.content" :key="bhajan.id + index">{{ line }}</p>
+          <p v-for="(line, index) in bhajan.content" :key="bhajan.id + index">
+            {{ line }}
+          </p>
         </div>
       </div>
     </div>
